e25b254f2b5c013f70560256fd62a200001f2311
efrainjara91 <efrainjara91@gmail.com>
2023-05-19 20:58:53 -0400

    first commit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use( bodyParser() );
+
+app.use(cors({
+    origin:"*"
+}))
+
+const userRoute = require("./src/route/users/users.route");
+const themeRoute = require("./src/route/themes/themes.route");
+const topicRoute = require("./src/route/topics/topics.route");
+ 
+//Ruta raiz
+app.get('/', function (req, res) {
+    //Logica.
+    res.send('Hello World');
+});
+
+app.get('/pagina2', function (req, res) {
+    //Logica de negocios
+    //esta aqui -Controller
+
+    res.json({application: 'Study APP', version: '1.0.0'});
+});
+
+//Llamadas a los routes de los UCs
+userRoute(app);
+themeRoute(app);
+topicRoute(app);
+
+app.listen(3000);
